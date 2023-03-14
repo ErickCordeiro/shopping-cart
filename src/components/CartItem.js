24a1d81f3a812bb0cd93357363fd449a85a21f74
Erick Cordeiro <erickcordeiroa@gmail.com>
2023-03-14 01:42:39 -0300

    Creating shopping cart contextApi

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { id, title, image, price, amount } = item;
+
+  return (
+    <div className="flex">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px]" src={image} alt="" />
+        </Link>
+
+        <div className="flex flex-col w-full">
+          <div className="flex justify-between mb-2">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
+            <div className="text-xl cursor-pointer">
+              <IoMdClose className="text-gray-500 transition hover:text-red-500" />
+            </div>
+          </div>
+
+          <div className="bg-pink-200 flex gap-x-2 h-[36px] items-center">
+            <div className="bg-blue-300 flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
+              <div className="flex items-center justify-center flex-1 h-full cursor-pointer">
+                <IoMdRemove />
+              </div>
+              <div className="flex items-center justify-center h-full px-2">
+                {amount}
+              </div>
+              <div className="flex items-center justify-center flex-1 h-full cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+            <div>{item.price}</div>
+            <div>Final {item.price}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import CartItem from "../components/CartItem";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { CartContext } from "../contexts/CartContext";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isOpen, handleClose } = useContext(SidebarContext);
+  const { cart } = useContext(CartContext);
+
+  return (
+    <>
+      <div
+        onClick={handleClose}
+        className={`${
+          isOpen ? "bg-black/20 absolute" : "none bg-none"
+        }  top-0 left-0 z-10 w-full h-full`}
+      ></div>
+      <div
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20`}
+      >
+        <div className="flex items-center justify-between p-4 text-white bg-red-500">
+          <div className="text-sm font-semibold uppercase">Meu Carrinho (0)</div>
+          <div className="flex items-center justify-center w-8 h-8 cursor-pointer">
+            <IoMdArrowForward onClick={handleClose} className="text-2xl" />
+          </div>
+        </div>
+
+      <div className="p-4">
+        {cart.map(item => {
+          return <CartItem item={item} key={item.id} />
+        })}
+      </div>  
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
